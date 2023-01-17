28eff096f214491b40e34ec2d98e41e35a1a4956
LY-IENG Steven <steven.ly412@gmail.com>
2023-01-17 12:51:00 +0100

    2  ajouter une traduction

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <set>
+#include <string>
 using namespace std;
 int main()
 {
@@ -8,7 +9,12 @@ int main()
     while (!exit.count(input))
     {
         cout << "This is a translator, but it does nothing for now..." << endl;
-        cin >> input;
+        getline(cin,input);
+        if( input.find("add") == 0)
+        {
+            cout << "added" << endl;
+            break;
+        }
     }
     cout << "sortie du programme" << endl;
     return 0;
