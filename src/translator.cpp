5ac94e11d8cb746e98fe5daff4dfab1e02bc4d83
LY-IENG Steven <steven.ly412@gmail.com>
2023-01-21 20:17:00 +0100

    2 ajouter une traduction (fin)

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,19 +1,64 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <map>
 using namespace std;
 int main()
 {
     set<string> exit = {"q","e","quit","exit"};
+    map<string,string> translation;
     string input =" ";
-    while (!exit.count(input))
+    while (true)
     {
-        cout << "This is a translator, but it does nothing for now..." << endl;
+        //cout << "This is a translator, but it does nothing for now..." << endl;
         getline(cin,input);
-        if( input.find("add") == 0)
+        if(exit.count(input)){
+            return 0;
+        }
+        else if( input.find("add") == 0)
         {
-            cout << "added" << endl;
-            break;
+            stringstream s(input);
+            string word;
+            vector<string> words; 
+            while(s >> word)
+            {
+                words.push_back(word);
+            }
+            cout << words[1] << " => " << words[2] << endl;
+            translation[words[1]] = words[2];
+        }
+        else if(input.find("translate") == 0){
+            stringstream s(input);
+            string word;
+            vector<string> words;
+            while(s >> word)
+            {
+                words.push_back(word);
+            }
+            int vecsize = words.size();
+            for(auto i = 1 ; i!= vecsize ; i++)
+            {
+                auto it = translation.find(words[i]);
+                if(it != translation.end())
+                {
+                    cout << it->second << " ";
+                }
+                else
+                {
+                    cout << words[i] << " ";
+                }
+                
+            }
+            cout << endl;
+        }
+        else if (input.find("print") == 0)
+        {
+            for(auto trwords : translation)
+            {
+                cout << trwords.first << " : " << trwords.second << endl;
+            }
         }
     }
     cout << "sortie du programme" << endl;
