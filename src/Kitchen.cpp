12971ce0024aa3ca4f88bcabf19d8e1d946d18de
LY-IENG Steven <steven.ly412@gmail.com>
2023-03-14 12:45:04 +0100

    test02 en cours

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,44 @@
 #include "Kitchen.hpp"
+#include "../lib/Unit.hpp"
+#include "../lib/Ingredient.hpp"
+#include <string>
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    auto [it,emplaced] = _units.emplace(std::move(unit));
+    return *it;
+}
+
+const Unit * Kitchen::find_unit(std::string& unit_find) const
+{
+    auto it = _units.find(Unit{unit_find});
+    if(it != _units.end())
+    {
+        return &(*it);
+    }
+    return nullptr;
+}
+
+const Ingredient& Kitchen::register_ingredient(Ingredient ingredient)
+{
+    auto [it,emplaced] = _ingredient.emplace(std::move(ingredient));
+    return *it;   
+}
+
+const Ingredient * Kitchen::find_ingredient(std::string ingred) const
+{
+
+    for(const auto &it : _ingredient)
+    {
+        auto name = it.name;
+        for(auto& c : name)
+        {
+            c = tolower(c);
+        }
+        if(name == ingred)
+        {
+            return &it;
+        }
+    }
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,34 @@
 #pragma once
+#include "../lib/Unit.hpp"
+#include "../lib/Ingredient.hpp"
+#include <string>
+#include <set>
+#include <optional>
+
+struct comparator{
+    bool operator() (const Unit& unit1,const Unit& unit2) const
+    {
+        return unit1.name < unit2.name;
+    }
+    
+    bool operator()(const Ingredient& ing1,const Ingredient& ing2) const
+    {
+        return ing1.name <ing2.name;
+    }
+};
 
 class Kitchen
-{};
+{   
+    private:
+        std::set<Unit,comparator> _units;
+        std::set<Ingredient,comparator> _ingredient;
+    public:
+        const Unit& register_unit(Unit unit);
+
+        const Unit* find_unit(std::string& unit_find) const;
+
+        const Ingredient& register_ingredient(Ingredient ingredient);
+        
+        const Ingredient* find_ingredient(std::string ingred)const;
+
+};
