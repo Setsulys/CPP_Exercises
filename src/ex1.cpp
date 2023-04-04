7645af632547833c71fc3b9485b470013a1f7aed
LY-IENG Steven <steven.ly412@gmail.com>
2023-04-04 11:54:53 +0200

    test 10-11-12 done

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,23 @@
 #include "ex1.hpp"
+#include "../../lib/concatenate.hpp"
+#include <list>
+
+void pairwise_concatenate(std::list<std::list<int>>& lst1,const std::list<std::list<int>> lst2)
+{
+        auto it = lst1.begin();
+        for(auto it2 = lst2.begin(); it2 != lst2.end(); it2++)
+        {
+            concatenate(*it,std::move(*it2));
+            it++;
+        }
+}
+
+void pairwise_concatenate(std::list<std::list<int>>& lst1, std::list<std::list<int>>&& lst2)
+{
+        auto it = lst1.begin();
+        for(auto it2 = lst2.begin(); it2 != lst2.end(); it2++)
+        {
+            concatenate(*it,std::move(*it2));
+            it++;
+        }
+}
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -1,1 +1,5 @@
 #pragma once
+#include <list>
+void pairwise_concatenate(std::list<std::list<int>>& lst1, const std::list<std::list<int>> lst2);
+
+void pairwise_concatenate(std::list<std::list<int>>& lst1, std::list<std::list<int>>&& lst2);
